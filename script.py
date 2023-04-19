ff7ee8873eaedf9baccf924cfd6d222e76a697b5
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-19 12:10:20 -0300

    Migrating a few deprecated dagster things to newest libraries

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from dagster import execute_pipeline, pipeline, solid, repository
+from dagster import JobDefinition, job, op, repository
 from modules.solids import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
-@pipeline
+@job
 def data_pipeline():
     df = create_dataframe()
     training_data, testing_data = split_data(vectorize_text(df),encode_label(df))
-    #The parts below could eventually be a second pipeline. if so, just comment the rest of this function, i.e. from line 14 to line 19. 
+    #The parts below could eventually be a second job. if so, just comment the rest of this function, i.e. from line 14 to line 19. 
     #for rf
     grid1 = prepare_grid.alias('prepare_grid_random_forest')
     #for log reg
@@ -18,11 +18,11 @@ def data_pipeline():
     process_model(training_data, testing_data,grid2())
     
 # If you want to create two pipelines: data preprocessing + Grid/train/test, uncomment the following block.
-# @pipeline
+# @job
 # def train_evaluate_pipeline(train_test_data):
     ##for more info on reusable solids -> see https://docs.dagster.io/tutorial/advanced_solids#reusable-solids
 
-    ##create aliases to apply different parameters where reusing a solid, i.e. grid_searCV()
+    ##create aliases to apply different parameters where reusing a op, i.e. grid_searCV()
     # optimized_model1 = grid_search.alias('prepare_grid_random_forest')
     # unoptimized_model = LogisticRegression(random_state=31)
     # optimized_model2 = grid_searchCV(unoptimized_model).alias('prepare_grid_logistic_regression')
@@ -37,8 +37,7 @@ def data_pipeline():
 
 
 if __name__ == "__main__":
-    execute_pipeline(data_pipeline)
-    #for the second pipeline, uncomment:
-    # execute_pipeline(train_evaluate_pipeline)
+    #for the first job, uncomment:
+    JobDefinition.execute_in_process(data_pipeline)
 
         

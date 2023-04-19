02f6885ae0269586bf8b3c6224bafb93a6d9d8c7
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-19 11:20:12 -0300

    initial setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Run:
+# chmod +x setup.sh
+# sudo ./setup.sh
+
+sudo apt-get install python3 python3-venv python3-pip -y
+
+git clone https://github.com/BrenoFariasdaSilva/MachineLearning-Dagster.git
+cd ML_with_DAGs
+pip install virtualenv
+python3 -m virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
